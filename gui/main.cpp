6adcc662bc52e4bebebb5aa01848c215e077678e
Lucien XU <sfietkonstantin@free.fr>
2013-03-07 14:02:11 +0100

    Small fix for booster

--- gui/main.cpp
+++ gui/main.cpp
@@ -10,10 +10,10 @@
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
-    qmlRegisterType<TestHelper>("org.SfietKonstantin.ambiancechanger", 1, 0, "TestHelper");
-    qmlRegisterType<HelperLauncher>("org.SfietKonstantin.ambiancechanger", 1, 0, "HelperLauncher");
     QScopedPointer<QApplication> app(Sailfish::createApplication(argc, argv));
     QScopedPointer<QDeclarativeView> view(Sailfish::createUninitializedView());
+    qmlRegisterType<TestHelper>("org.SfietKonstantin.ambiancechanger", 1, 0, "TestHelper");
+    qmlRegisterType<HelperLauncher>("org.SfietKonstantin.ambiancechanger", 1, 0, "HelperLauncher");
     Sailfish::initializeView(view.data(), "main.qml");
     Sailfish::showView(view.data());
 
